bcdce9c5de267c18ddc6dadc99d5d0543bdc0b5b
emmanuelc1 <emmanuelcastillo1@gmail.com>
2023-09-14 17:32:11 -0700

    fixed bug where useContext had AuthProvider instead of AuthContext

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -49,7 +49,7 @@ function AuthProvider({ children }) {
 }
 
 function useAuth() {
-  const context = useContext(AuthProvider);
+  const context = useContext(AuthContext);
   if (context === undefined)
     throw new Error('AuthContext was used outside the AuthProvider');
   return context;
